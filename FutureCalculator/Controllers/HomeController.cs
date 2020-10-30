7a7d23c4eb7a07cd0d9cb183712634da6e0c72ba
Netochukwu Anyankah <nanyankah@gmail.com>
2020-10-30 04:38:49 +0100

    Add Models, views and controllers

---
+++ FutureCalculator/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FutureCalculator.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FutureCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FutureValue = null;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FutureValueModel fv)
+        {
+            if (ModelState.IsValid)
+            {
+            ViewBag.FutureValue = fv.Calculate().ToString("c2");
+           
+            }
+            else
+            {
+                ViewBag.Futurevalue = null;
+            }
+
+            return View();
+        }
+    }
+}
--- FutureCalculator/Models/FutureValueModel.cs
+++ FutureCalculator/Models/FutureValueModel.cs
@@ -7,5 +7,21 @@ namespace FutureCalculator.Models
 {
     public class FutureValueModel
     {
+        public decimal MonthlyInvestment { get; set; }
+        public decimal  YearlyInterestRate { get; set; }
+        public int Years { get; set; }
+
+        public decimal  Calculate() {
+            int months = Years * 12;
+            decimal monthltInterestRate = YearlyInterestRate / 12 / 100;
+            decimal futureValue = 0;
+
+            for (var  i = 0; i< months; i++)
+            {
+                futureValue = (futureValue + MonthlyInvestment) * (1 + monthltInterestRate);
+            }
+
+            return futureValue;
+        }
     }
 }
